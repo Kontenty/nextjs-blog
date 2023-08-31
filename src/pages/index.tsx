995eec2c3d39b8cc9d4c18bfe5fd4eb1aefacea8
Konrad Gorski <konrad.gorski@idea.edu.pl>
2023-08-31 18:43:17 +0200

    feat: add title on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,6 +20,11 @@ export default function Home({ posts }: Props) {
   return (
     <main className={`flex min-h-screen p-12 ${inter.className}`}>
       <div className="max-w-5xl w-full mx-auto">
+        <h1 className="mb-4 text-center text-3xl">From the blog</h1>
+        <h3 className="mb-8 max-w-md mx-auto text-center text-slate-400 text-lg">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus
+          dignissimos magnam rerum debitis iste voluptatem libero a.
+        </h3>
         <div className="grid grid-cols-3 gap-x-5 gap-y-6">
           {posts.map((post) => (
             <Post key={post.id} post={post} />
